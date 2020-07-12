70007b8a4dd745cec79bc7e99a64d9b1f27142d5
bard.bringeland@gmail.com <bard.bringeland@gmail.com>
2020-07-12 23:20:13 +0200

    Updates

---
+++ precache-manifest.e988902eb7dedba019a98446d72c71c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71dcc5f00fe51a18fc91a9ec592f58b",
+    "url": "/flag/index.html"
+  },
+  {
+    "revision": "97e50a4eddc56e6bf656",
+    "url": "/flag/static/css/main.ba19ed3c.chunk.css"
+  },
+  {
+    "revision": "4f617a6193312ba1cda9",
+    "url": "/flag/static/js/2.c71a6230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flag/static/js/2.c71a6230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e50a4eddc56e6bf656",
+    "url": "/flag/static/js/main.8ef6cb2f.chunk.js"
+  },
+  {
+    "revision": "0742e34d5b03b7988105",
+    "url": "/flag/static/js/runtime-main.5b39d9d8.js"
+  }
+]);
